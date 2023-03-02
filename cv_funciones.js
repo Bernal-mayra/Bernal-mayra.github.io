d4c321b7432fe65f9706c97a541b2116aeaf9eec
Mayra Andrea Bernal <123322979+Bernal-mayra@users.noreply.github.com>
2023-03-01 21:30:06 -0300

    initial commit

---
+++ cv_funciones.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+function bprest() {
+
+        document.getElementById('prest').style.display='block';
+}
+    function vermenosP() {
+        document.getElementById('prest').style.display='none';        
+    }
+    
+function bapt() {
+    document.getElementById('list-apt').style.display='block'
+}
+    function vermenosA() {
+        document.getElementById('list-apt').style.display='none';
+    }  
+
+function bexlab() {
+    document.getElementById('list-exlab').style.display='block'
+}
+    function vermenosEl() {
+        document.getElementById('list-exlab').style.display='none';
+    }  
+function bestud() {
+    document.getElementById('list-estud').style.display='block'
+}
+    function vermenosEs() {
+        document.getElementById('list-estud').style.display='none';
+    } 
+function bcursos() {
+    document.getElementById('list-curs').style.display='block'
+}
+    function vermenosC() {
+        document.getElementById('list-curs').style.display='none';
+    } 
+function mensajefinal() {
+    document.getElementById("demo").innerHTML = "Gracias por interesarse, cualquier inquietud comunicarse via email";
+}
